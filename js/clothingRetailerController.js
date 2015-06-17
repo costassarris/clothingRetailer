8ba942ab797b2c42a97a05af9fe8eb4d38740864
costassarris <costassarris@gmail.com>
2015-06-17 15:15:48 +0100

    calculates total price of items in shopping basket

--- js/clothingRetailerController.js
+++ js/clothingRetailerController.js
@@ -1,9 +1,22 @@
 clothingRetailer.controller('ClothingRetailerController', [function() {
 
-  this.shoppingCart = [];
+  this.basket = [];
+  this.totalPrice = 0.00;
 
   this.addItem = function(item) {
-    this.shoppingCart.push(item);
+    this.basket.push(item);
+  };
+
+  this.removeItem = function(item) {
+    var index = this.basket.indexOf(item);
+    this.basket.splice(index, 1);
+  };
+
+  this.calculateTotal = function() {
+    for (var i=0; i<this.basket.length; i++) {
+      var currentItem = this.basket[i];
+      this.totalPrice += currentItem.price;
+    }
   };
 
 }]);
--- test/clothingRetailerController.spec.js
+++ test/clothingRetailerController.spec.js
@@ -5,16 +5,31 @@ describe('ClothingRetailerController', function() {
 
   beforeEach(inject(function($controller) {
     shop = $controller('ClothingRetailerController');
-    shop.shoes = { "name" : "Suede Shoes, Blue", "price" : 42.00, "available" : 4, "gender" : "women", "category" : "footwear" }
+    shop.shoes = { "name" : "Shoes", "price" : 42.00, "available" : 4, "gender" : "women", "category" : "footwear" }
+    shop.dress = { "name" : "Dress", "price" : 55.00, "available" : 7, "gender" : "women", "category" : "formal" }
   }));
 
-  it ('initialises with an empty shopping cart', function() {
-    expect(shop.shoppingCart).toEqual([])
+  it ('initialises with an empty shopping basket', function() {
+    expect(shop.basket).toEqual([])
   });
 
-  it ('allows user to add an item to the shopping cart', function() {
+  it ('allows user to add an item to the shopping basket', function() {
     shop.addItem(shop.shoes);
-    expect(shop.shoppingCart).toEqual([shop.shoes]);
+    expect(shop.basket).toEqual([shop.shoes]);
+  });
+
+  it ('allows user to remove item from the shopping basket', function() {
+    shop.addItem(shop.shoes);
+    shop.addItem(shop.dress);
+    shop.removeItem(shop.shoes);
+    expect(shop.basket).toEqual([shop.dress]);
+  });
+
+  it ('displays the total price for the products in the shopping basket', function() {
+    shop.addItem(shop.shoes);
+    shop.addItem(shop.dress);
+    shop.calculateTotal();
+    expect(shop.totalPrice).toEqual(97.00);
   });
 
 
